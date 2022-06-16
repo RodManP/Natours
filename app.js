8528067b8cf8f7aa2f55ab1c6cd3995fd5e15492
RodManP <manuel@empacotecnic.com>
2022-06-16 10:01:04 -0500

    end of lesson 115

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,10 +18,10 @@ app.use(express.json());
 // app.use(express.static(`${__dirname}/public`))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  console.log('hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('hello from the middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -29,5 +31,20 @@ app.use((req, res, next) => {
 // 2 ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can not find ${req.originalUrl}`,
+  // });
+
+  // const err = new Error(`can not find ${req.originalUrl}`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`can not find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
