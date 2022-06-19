8e953dda2375048f667e47b743212e0340894c17
RodManP <manuel@empacotecnic.com>
2022-06-19 02:31:16 -0500

    end of lesson 130

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,12 +19,38 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Provide a password confirmation'],
+    validate: {
+      // only works on CREATE or SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords dont match',
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete the passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const { signup, login } = require('../controllers/authController');
 
-const{ getAllUsers, getUser, createUser, updateUser, deleteUser }= require('../controllers/userController')
-
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
 //  USER ROUTES
 const router = express.Router();
 
+router.post('/signup', signup);
+router.post('/login', login);
 
 router.route('/').get(getAllUsers).post(createUser);
 
-router
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
