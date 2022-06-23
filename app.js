4692b3867b1918df3acd0844794fbca0100e7f54
RodManP <manuel@empacotecnic.com>
2022-06-22 21:22:12 -0500

    end of section 10

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,20 @@ app.use(mongoSanitize());
 
 // DATA SANITIZATION against XSS
 app.use(xss());
+
+//Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 //SERVING STATIC FILES
 // app.use(express.static(`${__dirname}/public`))
