6f1846bb6f311de783dec6734b65836e0f082f8e
RodManP <manuel@empacotecnic.com>
2022-07-11 00:26:01 -0500

    end of section 12, lesson 197

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,19 +2,22 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type is either data or password
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data Updated Successfully');
+      showAlert('success', `${type.toUpperCase()} Updated Successfully`);
     }
   } catch (error) {
     showAlert('error', error.response.data.message);
