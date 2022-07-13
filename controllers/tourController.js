92232511b9b2ed834f327f0491291c320cc43de0
RodManP <manuel@empacotecnic.com>
2022-07-12 22:25:42 -0500

    end of lesson 205

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,10 +21,33 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 
-exports.resizeTourImages = (req, res, next) => {
-  console.log(req.files);
-  next()
-}
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1 Cover Image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2 Images
+  await Promise.all( req.files.images.map(async (file, index) => {
+     req.body.images =[]
+    const filename = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+
+    await sharp(file.buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/tours/${filename}`);
+
+    req.body.images.push(filename)
+  }));
+
+  next();
+});
 
 exports.uploadTourImages = upload.fields([
   { name: 'imageCover', maxCount: 1 },
