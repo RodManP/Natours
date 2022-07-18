5edb0fe510b470fa318e2785a1c02b572a32fc58
RodManP <manuel@empacotecnic.com>
2022-07-17 21:52:38 -0500

    Prepare app for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -25,17 +25,15 @@ export const login = async (email, password) => {
 };
 
 export const logout = async () => {
-  
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://localhost:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
 
     console.log(res.data.status);
 
     if (res.data.status === 'success') location.reload(true);
- 
   } catch (error) {
     alert('error', 'Error logging out');
   }
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { showAlert } from './alerts'
+import { showAlert } from './alerts';
 const stripe = Stripe(
   'pk_test_51LLpCxGncPXaAbkqHP7WP1R807kwSuJnNLTR8NWOVklw7Hlwee48FX9Nd1Qdbz4BWFUqecp8db4zJYAiYbMHpq2o00g11xoy6F'
 );
@@ -7,17 +7,14 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1 get checkout session from the server (API)
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    //console.log(session);
     // 2 create checkout form and charge card
-        await stripe.redirectToCheckout({
-            sessionId: session.data.session.id
-        })
-
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
   } catch (error) {
     console.log(error);
-    showAlert('error', error)
+    showAlert('error', error);
   }
 };
